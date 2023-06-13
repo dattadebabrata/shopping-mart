af43fef9e6fb7e2748665f9358e85daca0cb91a7
im-Debabrata <debabrata@tericsoft.com>
2023-06-13 11:39:38 +0530

    setup redux store

---
+++ src/store/cart/cart.reducer.ts
@@ -0,0 +1,8 @@
+import { AnyAction } from "redux";
+const INITIAL_STATE = {
+  cart: [],
+};
+
+export const cartReducer = (state = INITIAL_STATE, action: AnyAction) => {
+  return state;
+};
---
+++ src/store/store.ts
@@ -0,0 +1,32 @@
+import { compose, createStore, applyMiddleware, Middleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import logger from "redux-logger";
+
+import { rootReducer } from "./root-reducer";
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  sagaMiddleware,
+].filter((middleware): middleware is Middleware => Boolean(middleware));
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, composedEnhancers);
+
+// sagaMiddleware.run(rootSaga);
